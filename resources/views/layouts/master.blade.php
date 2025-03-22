140b43da89e7070ceb891625e40e9bf92f4ae9c5
usf54 <youssefoumhdi12@gmail.com>
2025-03-22 02:01:58 +0000

    adjust the cart icon and profile UI

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -43,65 +43,34 @@
           @if (!Auth::user())
             <div class="icon"><a href="{{route('profile.edit')}}"><i class="fa-regular fa-user"></i></a></div>
           @else
-            <div class="icon">
-                <a href="{{ route('cart.view') }}">
-                    <i class="fa-solid fa-cart-shopping"></i>
-                    <span id="cart-count">{{ session('cart') ? count(session('cart')) : 0 }}</span>
-                </a>
-            </div>
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white focus:outline-none transition ease-in-out duration-150">
-                                    <div>{{ Auth::user()->name }}</div>
-        
-                                    <div class="ms-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
-        
-                            <x-slot name="content">
-                                <x-dropdown-link :href="route('profile.edit')">
-                                    {{ __('Profile') }}
-                                </x-dropdown-link>
-        
-                                <!-- Authentication -->
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-        
-                                    <x-dropdown-link :href="route('logout')"
-                                            onclick="event.preventDefault();
-                                                        this.closest('form').submit();">
-                                        {{ __('Log Out') }}
-                                    </x-dropdown-link>
-                                </form>
-                            </x-slot>
-                        </x-dropdown>
-                    </div>
-            </div>
+          <div class="icon position-relative me-3">
+            <a href="{{ route('cart.view') }}" class="text-dark text-decoration-none">
+                <i class="fa-solid fa-cart-shopping fs-5"></i>
+                <span id="cart-count" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    {{ session('cart') ? count(session('cart')) : 0 }}
+                </span>
+            </a>
+        </div>
+
+        <div class="dropdown">
+            <button class="btn btn-light dropdown-toggle d-flex align-items-center" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                <span class="me-2">{{ Auth::user()->name }}</span>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Profile</a></li>
+                <li>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button class="dropdown-item" type="submit">Log Out</button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+
           @endif
       </div>
     </header>
-    <!-- Navigation Menu -->
-    <div class="links-container">
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item"><a class="nav-link" href="{{ route('products.list', ['category' => 'fruits']) }}">Fruits</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('products.list', ['category' => 'vegetables']) }}">Vegetables</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('products.list', ['category' => 'Meats']) }}">Meat & Chicken</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('products.list', ['category' => 'Bakery']) }}">Bakery & Bread</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('products.list', ['category' => 'seafood']) }}">Seafood</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
-
+   
     <main>
       @yield('content')
     </main>
