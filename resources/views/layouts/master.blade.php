7d199352e2248fadf0c02ccea4ff24a72d4b2025
usf54 <youssefoumhdi12@gmail.com>
2025-04-10 22:37:13 +0100

    add return to top btn

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -138,6 +138,12 @@
           <a class="text-white" href="#">GroceryHub</a>
         </div>
     </footer>
+    
+    <!-- Return to Top Button -->
+    <div class="return-to-top" id="returnToTop">
+        <i class="fas fa-arrow-up"></i>
+    </div>
+
     <script>
       document.addEventListener('DOMContentLoaded', function () {
           const searchInput = document.getElementById('live-search');
@@ -184,6 +190,24 @@
               }
           });
       });
+      const returnToTopButton = document.getElementById('returnToTop');
+          
+          // Show/hide button based on scroll position
+          window.addEventListener('scroll', function() {
+              if (window.pageYOffset > 300) {
+                  returnToTopButton.classList.add('show');
+              } else {
+                  returnToTopButton.classList.remove('show');
+              }
+          });
+          
+          // Smooth scroll to top when clicked
+          returnToTopButton.addEventListener('click', function() {
+              window.scrollTo({
+                  top: 0,
+                  behavior: 'smooth'
+              });
+          });
     </script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
