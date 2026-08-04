29a6e1fcafc699224b8359c866debd330262a5e0
youssef <youssefoumhdi12@gmail.com>
2026-08-04 19:28:21 +0100

    Add security headers middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => App\Http\Middleware\AdminMiddleware::class,
             'driver' => App\Http\Middleware\DriverMiddleware::class,
         ]);
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
 
